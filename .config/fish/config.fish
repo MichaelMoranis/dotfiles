8579f4a0135345fc5e94b6d5c12d86d01189d4db
michaelmoranis <michaelmoranesul@gmail.com>
2024-10-11 17:56:03 -0300

    Adds Fish Shell and LazyVim settings

---
+++ .config/fish/config.fish
@@ -0,0 +1,23 @@
+if status is-interactive
+    alias ll="exa -l -g --icons"
+    # Commands to run in interactive sessions can go here
+    # starship init fish | source
+    #starship init fish | source
+end
+
+# fnm
+set FNM_PATH "/home/michael/.local/share/fnm"
+if [ -d "$FNM_PATH" ]
+    set PATH "$FNM_PATH" $PATH
+    fnm env | source
+end
+
+# Avaliar as cores padrão do dircolors
+if test -e ~/.dircolors
+    eval (dircolors -c ~/.dircolors)
+else
+    eval (dircolors -c)
+end
+alias vim="nvim"
+alias ta="tmux attach"
+alias t="tmux"
